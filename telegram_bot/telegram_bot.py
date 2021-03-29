aac90a44315c710ff345ca871948b73e5ea38a72
vacoff <yusifysf2003@gmail.com>
2021-03-29 16:36:19 +0400

    Add project files.

---
+++ telegram_bot/telegram_bot.py
@@ -0,0 +1,37 @@
+from telegram.ext import Updater, CommandHandler
+import telegram
+import requests
+import re
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text("Hi!, use /help command for more information")
+
+def help(update, context):
+    mesg = " It is a bot that uses /gimmedoge command to give random cute dog images"
+    update.message.reply_text(mesg)
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents[ "url" ]
+    return url
+
+def gimmedoge(update, context):
+    bot = telegram.Bot(token = "1715900112:AAG4nBwyRHugzDAu5FKoRo-xzIoNOkb-5EY")
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id= chat_id, photo = url)
+
+def main():
+    updater = Updater("1715900112:AAG4nBwyRHugzDAu5FKoRo-xzIoNOkb-5EY",use_context = True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("gimmedoge",gimmedoge))
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("help",help))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
+
